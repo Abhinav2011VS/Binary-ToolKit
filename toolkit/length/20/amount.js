f5cd0088b751eb4aa8c5c1db5f0c2976673b36c1
Abhinav2011VS <152204613+Abhinav2011VS@users.noreply.github.com>
2024-06-04 18:57:50 +0530

    Create amount.js

---
+++ toolkit/length/20/amount.js
@@ -0,0 +1,5 @@
+// Update character count display
+document.getElementById("input").addEventListener("input", function() {
+    var inputLength = this.value.length;
+    document.getElementById("charCount").textContent = inputLength + "/20";
+});
