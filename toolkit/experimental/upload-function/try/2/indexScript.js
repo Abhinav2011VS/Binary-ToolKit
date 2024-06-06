8a40d4739721168ad06441401b2874ee5cc4b983
Abhinav2011VS <152204613+Abhinav2011VS@users.noreply.github.com>
2024-06-06 12:34:06 +0530

    Update indexScript.js

--- toolkit/experimental/upload-function/try/2/indexScript.js
+++ toolkit/experimental/upload-function/try/2/indexScript.js
@@ -24,26 +24,3 @@ function downloadBinaryFile(binaryData, fileName) {
 
     URL.revokeObjectURL(url);
 }
-
-// Binary to File Conversion
-function convertToOriginal() {
-    const binaryFileInput = document.getElementById('binaryFileInput');
-
-    const file = binaryFileInput.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function(event) {
-        const binaryData = event.target.result;
-        const blob = new Blob([binaryData], { type: file.type });
-        const url = URL.createObjectURL(blob);
-
-        const downloadLink = document.createElement('a');
-        downloadLink.href = url;
-        downloadLink.download = file.name;
-        downloadLink.click();
-        
-        URL.revokeObjectURL(url);
-    };
-
-    reader.readAsArrayBuffer(file);
-}
